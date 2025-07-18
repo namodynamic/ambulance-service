1320b01215f3daeb608e1b2e6b0894eae6a75914
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-07-18 19:36:07 +0100

    feat: Add environment variable support

--- src/main/java/com/ambulance/ambulance_service/AmbulanceServiceApplication.java
+++ src/main/java/com/ambulance/ambulance_service/AmbulanceServiceApplication.java
@@ -3,10 +3,15 @@ package com.ambulance.ambulance_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AmbulanceServiceApplication {
 
 	public static void main(String[] args) {
+		// Load .env file before Spring starts
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(AmbulanceServiceApplication.class, args);
 	}
 
